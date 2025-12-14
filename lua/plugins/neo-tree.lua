ca4405af02ff05d49d030fdfcb98575986301501
akatekhanh <akatekhanh0212@gmail.com>
2025-12-14 10:18:19 +0700

    change lazy git icon

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,6 +1,21 @@
 return {
   "neo-tree.nvim",
   opts = {
+    default_component_configs = {
+      git_status = {
+        symbols = {
+          added     = "✚",  -- plus: new file added to git
+          modified  = "",  -- pencil: file has changes
+          deleted   = "✖",  -- x mark: file removed
+          renamed   = "➜",  -- arrow: file moved/renamed
+          untracked = "★",  -- star: new file not yet tracked
+          ignored   = "◌",  -- dotted circle: git ignored
+          unstaged  = "○",  -- empty circle: changes not staged
+          staged    = "●",  -- filled circle: ready to commit
+          conflict  = "",  -- warning: merge conflict
+        },
+      },
+    },
     filesystem = {
       filtered_items = {
         visible = false, -- hide filtered items on open
